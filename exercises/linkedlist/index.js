d24e07547987de8f1b120f18799c8344dd296e57
Xianyi Liang <liangxianyi2011@gmail.com>
2023-10-08 17:31:19 -0700

    Test

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,129 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor () {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size () {
+    let count = 0;
+    let pointer = this.head;
+    while (pointer !== null) {
+      count++;
+      pointer = pointer.next;
+    }
+    return count;
+  }
+
+  getFirst () {
+    return this.head;
+  }
+
+  getLast () {
+    let pointer = this.head;
+    while (pointer.next) {
+      pointer = pointer.next;
+    }
+    return pointer;
+  }
+
+  clear () {
+    this.head = null;
+  }
+
+  removeFirst () {
+    if (this.head === null) return;
+    this.head = this.head.next;
+  }
+
+  removeLast () {
+    if (this.head === null) return;
+    let previous = this.head;
+    if (previous.next === null) {
+      this.removeFirst();
+      return;
+    }
+    let pointer = previous.next;
+    while (pointer && pointer.next !== null) {
+      previous = previous.next;
+      pointer = pointer.next;
+    }
+    previous.next = null;
+  }
+
+  insertLast (data) {
+    const nodeToAdd = new Node(data);
+    if (this.head === null) {
+      this.head = nodeToAdd;
+    } else {
+      const lastNode = this.getLast();
+      lastNode.next = nodeToAdd;
+    }
+
+  }
+
+  getAt (index) {
+    if (index < 0) return null;
+    let pointer = this.head;
+    let count = 0;
+    while (count < index) {
+    if (pointer === null) return null;
+    pointer = pointer.next;
+    count++;
+
+    }
+    return pointer;
+  }
+
+  removeAt (index) {
+    if (index === 0) {
+      if (this.head) this.head = this.head.next;
+      return;
+    }
+    const prevNode = this.getAt(index - 1);
+    if (prevNode === null || prevNode.next === null) return;
+    prevNode.next = prevNode.next.next;
+  }
+
+  insertAt (data, index) {
+    const newNode = new Node(data);
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
+    }
+    const prevNode = this.getAt(index - 1);
+    if (prevNode === null) {
+      this.getLast().next = newNode;
+      return;
+    };
+    newNode.next = prevNode.next;
+    prevNode.next = newNode;
+
+  }
+
+  forEach (fn) {
+    let pointer = this.head;
+    while (pointer) {
+      fn(pointer);
+      pointer = pointer.next;
+    }
+  }
+
+
+
+}
 
 module.exports = { Node, LinkedList };
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var charsMap = {};
+  for (let i = 0; i < str.length; i++)
+  {
+    var char = str[i];
+    if (charsMap.hasOwnProperty(char))
+    {
+      charsMap[char] += 1;
+    } else
+    {
+      charsMap[char] = 1;
+    }
+  }
+
+  var maxChar = "";
+  var maxValue = 0;
+  for (char in charsMap)
+  {
+    if (charsMap[char] > maxValue)
+    {
+      maxChar = char;
+      maxValue = charsMap[char];
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
